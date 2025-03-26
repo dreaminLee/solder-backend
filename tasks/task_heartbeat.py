3f885431bc6a47bb0fb37850a6a5f0e6c3f48def
dreaminLee <dreaming_lee@outlook.com>
2025-03-26 14:31:52 +0800

    修改心跳任务

--- tasks/task_heartbeat.py
+++ tasks/task_heartbeat.py
@@ -1,8 +1,9 @@
 from modbus.client import modbus_client
 
+heartbeat = True
+
 def task_heartbeat():
-    heart_beat = modbus_client.modbus_read('xq', 574, 1)
-    if heart_beat == [True]:
+    if heartbeat:
         modbus_client.modbus_write('xq', [False], 574, 1)
     else:
         modbus_client.modbus_write('xq', [True], 574, 1)
