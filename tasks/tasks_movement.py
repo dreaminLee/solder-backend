d3a5d431063b31b292d5895a72a595915275258f
dreaminLee <dreaming_lee@outlook.com>
2025-03-26 22:06:34 +0800

    更新task_movement参数和注释

--- tasks/tasks_movement.py
+++ tasks/tasks_movement.py
@@ -1,47 +1,57 @@
-'''
+def task_move_in_area_to_cold_area(cold_area_empty: int):
+    """
     入库：从入柜区到冷藏区
-'''
-def task_move_in_area_to_cold_area():
-    pass
+    :param cold_area_empty: 当前未放置锡膏的冷藏区点位
+    """
 
 
-'''
+def task_move_cold_area_to_rewarm_area(solder_to_move: Solder, rewarm_area_empty: int):
+    """
     从冷藏区到回温区
-'''
-def task_move_cold_area_to_rewarm_area():
+    :param solder_to_move: 待移动的锡膏
+    :param rewarm_area_empty: 当前未放置锡膏的回温区点位
+    """
     pass
 
 
-'''
+def task_move_rewarm_area_to_cold_area(solder_to_move: Solder, cold_area_empty: int):
+    """
     从回温区到冷藏区
-'''
-def task_move_rewarm_area_to_cold_area():
+    :param solder_to_move: 待移动的锡膏
+    :param cold_area_empty:
+    """
     pass
 
 
-'''
+def task_move_rewarm_area_to_ready_area(solder_to_move: Solder, ready_area_empty: int):
+    """
     从回温区到待取区（自动搅拌，流程中需要搅拌）
-'''
-def task_move_rewarm_area_to_ready_area():
+    :param solder_to_move: 待移动的锡膏
+    :param ready_area_empty: 当前未放置锡膏的待取区点位
+    """
     pass
 
 
-'''
+def task_move_ready_area_to_cold_area(solder_to_move: Solder, cold_area_empty: int):
+    """
     从待取区到冷藏区
-'''
-def task_move_ready_area_to_cold_area():
+    :param solder_to_move: 待移动的锡膏
+    :param cold_area_empty: 当前未放置锡膏的冷藏区点位
+    """
     pass
 
 
-'''
+def task_move_out_from_rewarm_area(solder_to_move: Solder):
+    """
     从回温区出库（出库搅拌）
-'''
-def task_move_out_from_rewarm_area():
+    :param solder_to_move: 待移动的锡膏
+    """
     pass
 
 
-'''
+def task_move_out_from_ready_area(solder_to_move: Solder):
+    """
     从待取区出库（自动搅拌，此时已搅拌过）
-'''
-def task_move_out_from_ready_area():
+    :param solder_to_move: 待移动的锡膏
+    """
     pass
