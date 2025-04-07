4ac33b700e268a93d581a42f31b8a3697efb7adb
dreaminlee <dreaming_lee@outlook.com>
2025-04-07 11:33:57 +0800

    修改获取数据库session方式

--- tasks/task_freeze.py
+++ tasks/task_freeze.py
@@ -8,32 +8,30 @@ from modbus.modbus_addresses import ADDR_REGION_WAIT_START, ADDR_REGION_WAIT_END
 
 
 def task_freeze():
-    session = db_instance.get_session()
+    with db_instance.get_session() as db_session:
 
-    solder_stationID_list_cold   = [r.StationID for r in session.query(Solder.StationID).filter(Solder.StationID.between(  ADDR_REGION_COLD_START,   ADDR_REGION_COLD_END)).all()]
-    solder_stationID_list_rewarm = [r.StationID for r in session.query(Solder.StationID).filter(Solder.StationID.between(ADDR_REGION_REWARM_START, ADDR_REGION_REWARM_END)).all()]
-    solder_stationID_list_wait   = [r.StationID for r in session.query(Solder.StationID).filter(Solder.StationID.between(  ADDR_REGION_WAIT_START,   ADDR_REGION_WAIT_END)).all()]
-    logger.info(f"冷藏区已有锡膏的点位：{solder_stationID_list_cold}")
-    logger.info(f"回温区已有锡膏的点位：{solder_stationID_list_rewarm}")
-    logger.info(f"待取区已有锡膏的点位：{solder_stationID_list_wait}")
+        solder_stationID_list_cold   = [r.StationID for r in db_session.query(Solder.StationID).filter(Solder.StationID.between(  ADDR_REGION_COLD_START,   ADDR_REGION_COLD_END)).all()]
+        solder_stationID_list_rewarm = [r.StationID for r in db_session.query(Solder.StationID).filter(Solder.StationID.between(ADDR_REGION_REWARM_START, ADDR_REGION_REWARM_END)).all()]
+        solder_stationID_list_wait   = [r.StationID for r in db_session.query(Solder.StationID).filter(Solder.StationID.between(  ADDR_REGION_WAIT_START,   ADDR_REGION_WAIT_END)).all()]
+        logger.info(f"冷藏区已有锡膏的点位：{solder_stationID_list_cold}")
+        logger.info(f"回温区已有锡膏的点位：{solder_stationID_list_rewarm}")
+        logger.info(f"待取区已有锡膏的点位：{solder_stationID_list_wait}")
 
-    session.close()
+        """
+            冷藏区的锡膏状态设成0（不可取不可放）
+            回温区的锡膏状态设成5（异常）
+            待取区的锡膏状态设成5（异常）
+        """
+        station_status_cold   = [0 if stationID in   solder_stationID_list_cold else 1 for stationID in range(  ADDR_REGION_COLD_START,   ADDR_REGION_COLD_END+1)]
+        station_status_rewarm = [5 if stationID in solder_stationID_list_rewarm else 0 for stationID in range(ADDR_REGION_REWARM_START, ADDR_REGION_REWARM_END+1)]
+        station_status_wait   = [5 if stationID in   solder_stationID_list_wait else 0 for stationID in range(  ADDR_REGION_WAIT_START,   ADDR_REGION_WAIT_END+1)]
 
-    """
-        冷藏区的锡膏状态设成0（不可取不可放）
-        回温区的锡膏状态设成5（异常）
-        待取区的锡膏状态设成5（异常）
-    """
-    station_status_cold   = [0 if stationID in   solder_stationID_list_cold else 1 for stationID in range(  ADDR_REGION_COLD_START,   ADDR_REGION_COLD_END+1)]
-    station_status_rewarm = [5 if stationID in solder_stationID_list_rewarm else 0 for stationID in range(ADDR_REGION_REWARM_START, ADDR_REGION_REWARM_END+1)]
-    station_status_wait   = [5 if stationID in   solder_stationID_list_wait else 0 for stationID in range(  ADDR_REGION_WAIT_START,   ADDR_REGION_WAIT_END+1)]
+        bulk_len = 100
+        for bulk_start in range(0, len(station_status_cold), bulk_len):
+            modbus_client._client.write_registers(bulk_start + ADDR_REGION_COLD_START, station_status_cold[bulk_start : bulk_start+bulk_len if bulk_start+bulk_len < len(station_status_cold) else None], unit=0)
 
-    bulk_len = 100
-    for bulk_start in range(0, len(station_status_cold), bulk_len):
-        modbus_client._client.write_registers(bulk_start + ADDR_REGION_COLD_START, station_status_cold[bulk_start : bulk_start+bulk_len if bulk_start+bulk_len < len(station_status_cold) else None], unit=0)
+        for bulk_start in range(0, len(station_status_rewarm), bulk_len):
+            modbus_client._client.write_registers(bulk_start + ADDR_REGION_REWARM_START, station_status_rewarm[bulk_start : bulk_start+bulk_len if bulk_start+bulk_len < len(station_status_rewarm) else None], unit=0)
 
-    for bulk_start in range(0, len(station_status_rewarm), bulk_len):
-        modbus_client._client.write_registers(bulk_start + ADDR_REGION_REWARM_START, station_status_rewarm[bulk_start : bulk_start+bulk_len if bulk_start+bulk_len < len(station_status_rewarm) else None], unit=0)
-
-    for bulk_start in range(0, len(station_status_wait), bulk_len):
-        modbus_client._client.write_registers(bulk_start + ADDR_REGION_WAIT_START, station_status_wait[bulk_start : bulk_start+bulk_len if bulk_start+bulk_len < len(station_status_wait) else None], unit=0)
+        for bulk_start in range(0, len(station_status_wait), bulk_len):
+            modbus_client._client.write_registers(bulk_start + ADDR_REGION_WAIT_START, station_status_wait[bulk_start : bulk_start+bulk_len if bulk_start+bulk_len < len(station_status_wait) else None], unit=0)
--- tasks/task_scan.py
+++ tasks/task_scan.py
@@ -8,63 +8,62 @@ from models import User, Solder, SolderFlowRecord
 
 
 def task_scan():
+    with db_instance.get_session() as db_session:
 
-    scanner_req = modbus_client.modbus_read("jcq", ADDR_SCANNER_STATUS.REQ, 1)[0]
-    scanner_pos = modbus_client.modbus_read("jcq", ADDR_SCANNER_STATUS.POS, 1)[0]
+        scanner_req = modbus_client.modbus_read("jcq", ADDR_SCANNER_STATUS.REQ, 1)[0]
+        scanner_pos = modbus_client.modbus_read("jcq", ADDR_SCANNER_STATUS.POS, 1)[0]
 
-    if not scanner_req:
-        modbus_client.modbus_write("jcq", 0, ADDR_SCANNER_CONFIRM.RET, 1)
-        modbus_client.modbus_write("jcq", 0, ADDR_SCANNER_CONFIRM.POS, 1)
-        return
+        if not scanner_req:
+            modbus_client.modbus_write("jcq", 0, ADDR_SCANNER_CONFIRM.RET, 1)
+            modbus_client.modbus_write("jcq", 0, ADDR_SCANNER_CONFIRM.POS, 1)
+            return
 
-    barcode_scanned = scan()
-    open("res_asc.txt", "w", encoding="utf8").write(barcode_scanned)
-    parse_result = parse_barcode(barcode_scanned)
-    if not parse_result:
-        modbus_client.modbus_write("jcq", 2,           ADDR_SCANNER_CONFIRM.RET, 1)
+        barcode_scanned = scan()
+        open("res_asc.txt", "w", encoding="utf8").write(barcode_scanned)
+        parse_result = parse_barcode(barcode_scanned)
+        if not parse_result:
+            modbus_client.modbus_write("jcq", 2,           ADDR_SCANNER_CONFIRM.RET, 1)
+            modbus_client.modbus_write("jcq", scanner_pos, ADDR_SCANNER_CONFIRM.POS, 1)
+            return
+
+        model = parse_result['model']
+        # productDate = parse_result['product_date']
+        # expireDate = parse_result['expire_date']
+        # shelfLife = parse_result['shelf_life']
+
+
+        user_cache_file = "user_cache.txt"
+        user_id = int(open(user_cache_file, "r").read())
+        user_name = db_session.query(User.UserName).where(User.UserID == user_id).scalar()
+        user_name = user_name if user_name else "未知用户"
+
+        #查询此前该锡膏的入柜次数
+        in_times=db_session.query(SolderFlowRecord
+                        ).join(Solder, Solder.SolderCode==SolderFlowRecord.SolderCode
+                        ).filter(SolderFlowRecord.Type=="请求入柜"
+                        ).count()
+        new_solder = Solder(
+            SolderCode=barcode_scanned,
+            Model=model,
+            # ProductDate=productDate,
+            # ExpireDate=expireDate,
+            # ShelfLife = shelfLife,
+            InTimes=in_times+1,
+            BackLCTimes=0,
+            StationID=190,
+            StorageUser=user_name,
+            StorageDateTime=datetime.now()
+        )
+        new_solderflowrecord = SolderFlowRecord(
+            SolderCode=barcode_scanned,
+            UserID=user_id,
+            UserName=user_name,
+            DateTime=datetime.now(),
+            Type="请求入柜"
+        )
+        db_session.add(new_solder)
+        db_session.add(new_solderflowrecord)
+        db_session.commit()
+
+        modbus_client.modbus_write("jcq", 1,           ADDR_SCANNER_CONFIRM.RET, 1)
         modbus_client.modbus_write("jcq", scanner_pos, ADDR_SCANNER_CONFIRM.POS, 1)
-        return
-
-    model = parse_result['model']
-    # productDate = parse_result['product_date']
-    # expireDate = parse_result['expire_date']
-    # shelfLife = parse_result['shelf_life']
-
-    db_session = db_instance.get_session()
-
-    user_cache_file = "user_cache.txt"
-    user_id = int(open(user_cache_file, "r").read())
-    user_name = db_session.query(User.UserName).where(User.UserID == user_id).scalar()
-    user_name = user_name if user_name else "未知用户"
-
-    #查询此前该锡膏的入柜次数
-    in_times=db_session.query(SolderFlowRecord
-                      ).join(Solder, Solder.SolderCode==SolderFlowRecord.SolderCode
-                      ).filter(SolderFlowRecord.Type=="请求入柜"
-                      ).count()
-    new_solder = Solder(
-        SolderCode=barcode_scanned,
-        Model=model,
-        # ProductDate=productDate,
-        # ExpireDate=expireDate,
-        # ShelfLife = shelfLife,
-        InTimes=in_times+1,
-        BackLCTimes=0,
-        StationID=190,
-        StorageUser=user_name,
-        StorageDateTime=datetime.now()
-    )
-    new_solderflowrecord = SolderFlowRecord(
-        SolderCode=barcode_scanned,
-        UserID=user_id,
-        UserName=user_name,
-        DateTime=datetime.now(),
-        Type="请求入柜"
-    )
-    db_session.add(new_solder)
-    db_session.add(new_solderflowrecord)
-    db_session.commit()
-    db_session.close()
-
-    modbus_client.modbus_write("jcq", 1,           ADDR_SCANNER_CONFIRM.RET, 1)
-    modbus_client.modbus_write("jcq", scanner_pos, ADDR_SCANNER_CONFIRM.POS, 1)
